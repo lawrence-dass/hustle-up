30badc03aaba3123b54e5671adf08d7eaf1f4e5e
Lawrence <Lawrence4code@gmail.com>
2018-10-31 00:14:54 +0530

    Added Testimonial section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,4 @@ import '../scss/style.scss';
 console.log('This is from index.js file.');
 
 // text visibiity blur issue with plans section, card and card back.
+// backface - visibility: hidden; did not work, try something else.
