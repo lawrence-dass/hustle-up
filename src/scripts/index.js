d42a6f47ea2b62817b22d862da35dcd34428a981
Lawrence <Lawrence4code@gmail.com>
2018-11-01 10:05:41 +0530

    Completed contact section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,4 @@ console.log('This is from index.js file.');
 
 // text visibiity blur issue with plans section, card and card back.
 // backface - visibility: hidden; did not work, try something else.
+// Plan membership text padding issue.
