587bf4e5ce068b42b54dfeaff3b8267aad781097
Lawrence <Lawrence4code@gmail.com>
2018-12-22 02:13:57 +0530

    Using JS instead of CSS for popups and fixed styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,14 @@ import '../scss/style.scss';
 
 console.log('This is from index.js file.');
 
-// text visibiity blur issue with plans section, card and card back.
-// backface - visibility: hidden; did not work, try something else.
-// Plan membership text padding issue.
+const popup = document.querySelector('.popup');
+
+document.querySelector('.popup__close').addEventListener('click', () => {
+  popup.style.visibility = 'hidden';
+});
+
+window.addEventListener('click', e => {
+  if (e.target === popup) {
+    popup.style.display = 'none';
+  }
+});
